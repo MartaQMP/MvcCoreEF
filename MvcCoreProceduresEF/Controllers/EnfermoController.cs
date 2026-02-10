8fa7471cf1ce8080a6fb47fdaaa9fd53fc9341a3
MartaQMP <martaquirosmp@gmail.com>
2026-02-10 14:19:58 +0100

    10/02 dia

---
+++ MvcCoreProceduresEF/Controllers/EnfermoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreProceduresEF.Models;
+using MvcCoreProceduresEF.Repositories;
+
+namespace MvcCoreProceduresEF.Controllers
+{
+    public class EnfermoController : Controller
+    {
+        RepositoryEnfermo repo;
+
+        public EnfermoController (RepositoryEnfermo repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Enfermo> enfermos = await this.repo.GetEnfermosAsync();
+            if(enfermos == null)
+            {
+                ViewBag.Mensaje = "No hay enfermos";
+                return View();
+            }
+            return View(enfermos);
+        }
+    }
+}
---
+++ MvcCoreProceduresEF/Repositories/RepositoryEnfermo.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreProceduresEF.Data;
+using MvcCoreProceduresEF.Models;
+using System.Data.Common;
+using System.Data;
+
+#region STORED PROCEDURES
+/*
+--------------------
+--MOSTRAR ENFERMOS--
+--------------------
+    CREATE PROCEDURE SP_ALL_ENFERMOS
+    AS
+	    SELECT * FROM ENFERMO
+    GO
+
+---------------------
+--ENCONTRAR ENFERMO--
+---------------------
+    CREATE PROCEDURE SP_FIND_ENFERMO (@inscripcion nvarchar(50))
+    AS
+	    SELECT * FROM ENFERMO WHERE INSCRIPCION = @inscripcion
+    GO
+
+--------------------
+--ELIMINAR ENFERMO--
+--------------------
+    CREATE PROCEDURE SP_DELETE_ENFERMO (@inscripcion nvarchar(50))
+    AS
+	    DELETE FROM ENFERMO WHERE INSCRIPCION = @inscripcion
+    GO
+ */
+#endregion
+namespace MvcCoreProceduresEF.Repositories
+{
+    public class RepositoryEnfermo
+    {
+        EnfermoContext context;
+
+        public RepositoryEnfermo(EnfermoContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Enfermo>> GetEnfermosAsync()
+        {
+            /* NECESITAMOS UN COMMAND VAMOS A USAR UN using PARA TODO 
+            *  EL COMMAND EN SU CREACION NECESITA UNA CADENA DE CONEXION (OBJETO)
+            *  EL OBJETO CONNNECTION NOS LO OFRECE EF */
+            using (DbCommand com = this.context.Database.GetDbConnection().CreateCommand())
+            {
+                string sql = "SP_ALL_ENFERMOS";
+                com.CommandType = CommandType.StoredProcedure;
+                com.CommandText = sql;
+                await com.Connection.OpenAsync();
+                DbDataReader reader = await com.ExecuteReaderAsync();
+                // DEBEMOS MAPEAR LOS DATOS MANUALMENTE
+                List<Enfermo> enfermos = new List<Enfermo>();
+                while(await reader.ReadAsync())
+                {
+                    Enfermo enf = new Enfermo
+                    {
+                        Inscripcion = reader["INSCRIPCION"].ToString(),
+                        Apellido = reader["APELLIDO"].ToString(),
+                        Direccion = reader["DIRECCION"].ToString(),
+                        FechaNac = DateTime.Parse(reader["FECHA_NAC"].ToString()),
+                        Genero = reader["S"].ToString(),
+                        NSS = reader["NSS"].ToString()
+                    };
+                    enfermos.Add(enf);
+                }
+                await reader.CloseAsync();
+                await com.Connection.CloseAsync();
+                return enfermos;
+            }
+        }
+    }
+}
